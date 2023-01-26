c29f73678b2eed1f594e210788ff1ad106aea864
Farxodbek Ravshanbekov <62793789+HEllo1234567890-cloud@users.noreply.github.com>
2023-01-26 11:50:07 +0500

    prepared for render

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.set('view engine', 'ejs')
 app.use(bp.urlencoded({extended:true}))
 app.use(express.static('public'))
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log('On it..')
 })
 app.get('/',function(req,res){
